8a3b3e9a692d9145a484e8723a79aae8772ff757
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-01-21 19:55:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   elsif array.length == 1 
     array.join
   else array.length > 2 
-    array[-2] << " and "
+    array[-2].prepend " and "
     array.join(", ")
   end 
 end
